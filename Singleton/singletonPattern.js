5183d9f28b3f6fca1af4d394d0929be6d9951673
dineshdsv3 <dineshdsv3@gmail.com>
2023-07-27 12:50:37 +0530

    Singleton Patetrn

--- Singleton/singletonPattern.js
+++ Singleton/singletonPattern.js
@@ -1,4 +1,8 @@
-
+// We are declaring the static counter, instance variables and assigning the instance to the Singleton class,
+// instead of returning class we are returning the instance of SingleTon class in getInstance function
+// In the nextCOunterFunction we are returning the counter from the SingleTon
+// We are assigning the instance of SingleTon Class in single and single2 variables
+// As SingleTon is Anti-Pattern, It's not recommended to use Singleton Pattern.
 class SingleTon {
   static counter = 1;
   static instance = new SingleTon();
